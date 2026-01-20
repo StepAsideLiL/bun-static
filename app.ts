1fd5ed660a17c3c9757ecaa1af311fce32414ce9
Rifat Khan <54655388+StepAsideLiL@users.noreply.github.com>
2026-01-20 11:06:47 +0600

    generate html content from .md file.

--- app.ts
+++ app.ts
@@ -1,8 +1,28 @@
 import { file } from "bun";
+import { micromark } from "micromark";
+
+const HTML_TEMPLETE = `<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>
+    <!-- Title -->
+  </title>
+</head>
+
+<body>
+  <!-- Body -->
+</body>
+
+</html>`;
 
 async function main() {
   const rootFile = file("./site/index.md");
-  console.log(await rootFile.text());
+  console.log(
+    HTML_TEMPLETE.replace("<!-- Body -->", micromark(await rootFile.text())),
+  );
 }
 
 await main();
